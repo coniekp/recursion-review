336e3a0e9714c9776b9fbc280e28d18144450ce8
coniekp <coniekp@icloud.com>
2018-06-11 12:32:19 -0700

    "Defined stringifyJSON() and fill-me-in value"

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,37 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  if (typeof obj === 'function' || obj === undefined) {
+
+    return '';
+  }
+  var primitiveTypes = ['number', 'boolean'];
+  if (primitiveTypes.includes(typeof obj) || obj === null) {
+    return '' + obj;
+  }
+  
+  if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  }
+  
+  if (Array.isArray(obj)) {
+    var result = [];
+    obj.forEach(function (item) {
+      result.push(stringifyJSON (item));
+    });
+    
+    return '[' + result.join(',') + ']';
+  }
+  
+  if (typeof obj === 'object') {
+    var result = [];
+    for (var key in obj) {
+      if (obj[key] !== undefined && typeof obj[key] !== 'function') {
+        result.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key]));
+      }
+    }
+    
+    return '{' + result.join(',') + '}';
+  }
+
 };
