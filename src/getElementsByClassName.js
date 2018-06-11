34914c369a046347c7f372df405d03504434b59e
coniekp <coniekp@icloud.com>
2018-06-11 11:57:44 -0700

    "Defined getElementsByClassName() and added fill-me-in value"

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,5 +6,22 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className
 ) {
-  // your code here
+  var getElementsByClassName = function(className) {
+
+  var output = [];
+
+  var search = function(node) {
+    var node = node || document.body;
+    if (node.classList && node.classList.contains(className)) {
+      output.push(node);
+    }
+    for (var i = 0; i < node.children.length; i++) {
+      search(node.children[i]);
+    }
+  }
+
+  search();
+  return output;
+
 };
+
