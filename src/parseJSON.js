59fc569ed9a205a87391dc12e88d809f28c6cdca
coniekp <coniekp@icloud.com>
2018-06-11 16:26:06 -0700

    "Pseudocode and strategy for parser"

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -4,4 +4,34 @@
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
   // your code goes here
+  
 };
+
+
+
+/*
+getType(str, index) {
+  if (str === { ) {
+    makeObject (json.slice(index))
+  }
+  if (str == [){
+   makeArray();
+  }
+  makeValue();
+}
+
+
+makeObject(str) {
+  var key = '';
+  build a string until we see :
+  add this as key name
+  run getType on str after : and set that equal to key name
+  key = getType(obj[key])
+  
+}
+
+
+
+{a:0, b: {c:0}, d: {e:0}}
+
+*/
